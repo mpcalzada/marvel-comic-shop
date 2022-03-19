9e37010bbb1a3478b49f35139f007be3075a3581
Marco Calzada <mcalzada@kssoluciones.com>
2022-03-18 19:59:02 -0600

    feat(repository): add character/collaborator repositories
    Add integration with JPA for querying data using entities

---
+++ src/main/java/com/mcalzada/repository/CollaboratorRepository.java
@@ -0,0 +1,14 @@
+package com.mcalzada.repository;
+
+import com.mcalzada.model.entity.Character;
+import com.mcalzada.model.entity.Collaborator;
+import java.util.Optional;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface CollaboratorRepository extends JpaRepository<Collaborator, Integer>
+{
+
+    Optional<Collaborator> findFirstByName(String name);
+}
