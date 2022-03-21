d5da6c55814dfd71c4edc410a9d4def21bfb087b
Marco Calzada <mcalzada@kssoluciones.com>
2022-03-21 12:49:29 -0600

    fix(deploy): fix configuration for deploying services with docker
    fix application.properties with correct database configuration

--- assemble.sh
+++ assemble.sh
@@ -1,3 +1,6 @@
 #!/bin/bash
-#mvn package
+# Using maven for compile application
+# mvn package
+
+#Using docker for building application
 docker build -t mpcalzada/comics-api:1.0 .
--- avengers.sh
+++ avengers.sh
@@ -1,3 +1,6 @@
 #!/bin/bash
+# Using maven for running application
 #mvn spring-boot:run
+
+# Using
 docker-compose up -d
